90eb3a92abca0c774fee8f64c9b4257bf12783b3
ME1312 <admin@me1312.net>
2019-04-14 22:00:46 -0400

    Correct Protocol Post-Detection

--- Client/src/net/ME1312/SubData/Client/Protocol/Initial/InitPacketPostDeclaration.java
+++ Client/src/net/ME1312/SubData/Client/Protocol/Initial/InitPacketPostDeclaration.java
@@ -1,5 +1,6 @@
 package net.ME1312.SubData.Client.Protocol.Initial;
 
+import net.ME1312.Galaxi.Library.Container;
 import net.ME1312.Galaxi.Library.Map.ObjectMap;
 import net.ME1312.Galaxi.Library.Util;
 import net.ME1312.Galaxi.Library.Version.Version;
@@ -27,7 +28,7 @@ public final class InitPacketPostDeclaration implements InitialPacket, PacketObj
         Version version = data.getVersion(0x0002);
 
         if (Util.reflect(SubDataClient.class.getDeclaredField("state"), client) == ConnectionState.INITIALIZATION) {
-            if (client.getProtocol().getName() == name) {
+            if (new Container<>(client.getProtocol().getName()).equals(new Container<>(name))) {
                 List<Version> versions = Arrays.asList(client.getProtocol().getVersion());
                 if (versions.contains(version)) {
                     Util.reflect(DataClient.class.getDeclaredField("id"), client, clientID);
@@ -37,7 +38,7 @@ public final class InitPacketPostDeclaration implements InitialPacket, PacketObj
                     Util.reflect(SubDataClient.class.getDeclaredMethod("close", DisconnectReason.class), client, DisconnectReason.PROTOCOL_MISMATCH);
                 }
             } else {
-                DebugUtil.logException(new IllegalArgumentException("Protocol mismatch: [" + version + "] != [" + client.getProtocol().getName() + "]"), Util.reflect(SubDataProtocol.class.getDeclaredField("log"), client.getProtocol()));
+                DebugUtil.logException(new IllegalArgumentException("Protocol mismatch: [" + name + "] != [" + client.getProtocol().getName() + "]"), Util.reflect(SubDataProtocol.class.getDeclaredField("log"), client.getProtocol()));
                 Util.reflect(SubDataClient.class.getDeclaredMethod("close", DisconnectReason.class), client, DisconnectReason.PROTOCOL_MISMATCH);
             }
         }
